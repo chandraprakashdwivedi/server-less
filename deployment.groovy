f6567555a62b56424592055b9f9cd9682d7518ac
CP <chandra.prakash363@gmail.com>
2019-04-20 14:40:58 +0530

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -1,4 +1,5 @@
 node('master'){
+      checkout scm
       stage('Deploy using serverless framework'){
           def DeployCloudFormation = load "$JENKINS_HOME/ci/jenkins/script/deploy-stack.groovy"
           DeployCloudFormation.Deploy()
