1f3e2f711fc0a0dfd6ca636679f0f6947deb5c4d
CP <chandra.prakash363@gmail.com>
2019-04-20 14:15:48 +0530

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -1,20 +1,4 @@
-if(env.JOB_BASE_NAME == "master") {
-    env.ROLE_NAME="jenkins/appslave-jenkins-cpdelivery"
-    env.ENVIRONMENT="master"
-    env.MASTER_REGION="us-east-1"
-    env.ACCOUNT_NUMBER="815275185692"
-    env.VPC_NAME = "aws-ghns-sharedservices-prod-useast1-vpn"
-} else {
-    env.ROLE_NAME="jenkins/appslave-jenkins-cpdelivery"
-    env.ENVIRONMENT="development"
-    env.MASTER_REGION="us-east-1"
-    env.ACCOUNT_NUMBER="049723741716"
-    env.VPC_NAME = "aws-ghns-sharedservices-devl-useast1-cp"
+stage('Deploy using serverless framework'){
+  def DeployCloudFormation = load "ci/jenkins/scripts/deploy-stack.groovy"
+  DeployCloudFormation.Deploy()
 }
-
-withAWS(role:"${ROLE_NAME}", roleAccount: "${ACCOUNT_NUMBER}"){
-      stage('Deploy using serverless framework'){
-          def DeployCloudFormation = load "ci/jenkins/scripts/deploy-stack.groovy"
-          DeployCloudFormation.Deploy()
-      }
-    }
