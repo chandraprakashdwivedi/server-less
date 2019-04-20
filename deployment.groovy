7df6577a5184fc96fe6481040a3bacaaf1453069
CP <chandra.prakash363@gmail.com>
2019-04-20 15:01:29 +0530

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -2,7 +2,7 @@ node('master'){
       checkout scm
       stage('Deploy using serverless framework'){
           def DeployCloudFormation = load "$JENKINS_HOME/jobs/${env.JOB_NAME}/workspace/ci/jenkins/script/deploy-stack.groovy"
-          DeployCloudFormation.Deploy()
-          //DeployCloudFormation.Delete() //to delete stack
+          //DeployCloudFormation.Deploy()
+          DeployCloudFormation.Delete() //to delete stack
       }
     }
