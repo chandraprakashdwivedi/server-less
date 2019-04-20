3241c5552f78c6d229c580eb8f43763bec40c282
CP <chandra.prakash363@gmail.com>
2019-04-20 23:42:41 +0530

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -3,7 +3,7 @@ node('master'){
       stage('Deploy using serverless framework'){
           def DeployCloudFormation = load "$JENKINS_HOME/jobs/${env.JOB_NAME}/workspace/ci/jenkins/script/deploy-stack.groovy"
           def RemoveCloudFormation = load "$JENKINS_HOME/jobs/${env.JOB_NAME}/workspace/ci/jenkins/script/delete-stack.groovy"
-          //DeployCloudFormation.Deploy()
-          RemoveCloudFormation.Delete() //to delete stack
+          DeployCloudFormation.Deploy()
+          //RemoveCloudFormation.Delete() //to delete stack
       }
     }
