3772fb562c16432006c84818b2ab51c12e93efc7
CP <chandra.prakash363@gmail.com>
2019-04-22 18:50:45 +0530

    Update s3bucket.py

--- lambda_functions/s3bucket.py
+++ lambda_functions/s3bucket.py
@@ -4,12 +4,18 @@ import boto3
 s3 = boto3.resource('s3')
 
 def s3list(event,context):
+    print('event', event)
     my_bucket = s3.Bucket('test-sls-dev-uploads')
     bucket_obj = []
     
     for object in my_bucket.objects.all():
         print(object, object.bucket_name)
         bucket_obj.append(object.key)
+        
+   
+    sns = boto3.client('sns')
+    response = sns.publish(TopicArn='arn:aws:sns:ap-south-1:015878554706:test-sls-dev-topic',Message='This is test message from lambda')
+    print(response)
     
     response = {
         "statusCode": 200,
