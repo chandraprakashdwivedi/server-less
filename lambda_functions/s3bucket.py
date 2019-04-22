db8a84c8e3703b60ec6f3df16fa2230bcc7deeea
CP <chandra.prakash363@gmail.com>
2019-04-22 16:54:10 +0530

    Update s3bucket.py

--- lambda_functions/s3bucket.py
+++ lambda_functions/s3bucket.py
@@ -5,12 +5,16 @@ s3 = boto3.resource('s3')
 
 def s3list(event,context):
     my_bucket = s3.Bucket('test-sls-dev-uploads')
+    bucket_obj = []
     
     for object in my_bucket.objects.all():
-        print(object)
+        print(object, object.bucket_name)
+        bucket_obj.append(object.key)
     
     response = {
-        "statusCode": 200
+        "statusCode": 200,
+        "body": json.dumps(bucket_obj)
+        
     }
 
     return response
